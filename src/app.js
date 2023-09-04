3e52cbb39dc16dca328857b91dc9e604d107a049
jsosa26 <99664039+jsosa26@users.noreply.github.com>
2023-09-04 00:47:28 +0000

    facil y sencillo

--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+import { Tooltip } from "bootstrap";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const pintas = ["♠", "♣", "♥", "♦"];
+
+  const numeros = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  let randomNumber = numeros[Math.floor(Math.random() * numeros.length)];
+  let randomSymbol = pintas[Math.floor(Math.random() * pintas.length)];
+
+  document.getElementById("top").innerHTML = randomSymbol;
+  document.getElementById("number").innerHTML = randomNumber;
+  document.getElementById("bottom").innerHTML = randomSymbol;
 };
